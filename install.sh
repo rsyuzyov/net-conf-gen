271ee7fefd0c5b831fc69a6e68fd01f585fb66c9
Roman Syuzyov <rsyuzyov@gmail.com>
2025-12-09 10:29:50 +0500

    Установка зависимостей

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
- apt install python3 python3-pip python3-venv
- python3 -m venv venv
- source venv/bin/activate
- pip install -r requirements.txt
+apt install python3 python3-pip python3-venv libkrb5-dev libgssapi-krb5-2 -y
+python3 -m venv venv
+source venv/bin/activate
+pip install -r requirements.txt
